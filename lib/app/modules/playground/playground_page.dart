d0423d5c7ca7fbcad64bb4fa07f9ecc1be3c79a2
kconcha <kconcha@bancoguayaquil.com>
2023-12-23 11:54:28 -0500

    [Add] AnimatedContainer

--- lib/app/modules/playground/playground_page.dart
+++ lib/app/modules/playground/playground_page.dart
@@ -2,20 +2,52 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_como_un_pro/app/widgets/GraficoArco.dart';
-import 'package:flutter_como_un_pro/app/widgets/GraficoCirculo.dart';
 
-class PlaygroundPage extends StatelessWidget {
+class PlaygroundPage extends StatefulWidget {
   const PlaygroundPage({super.key});
+
+  @override
+  State<PlaygroundPage> createState() => _PlaygroundPageState();
+}
+
+class _PlaygroundPageState extends State<PlaygroundPage> {
+  Color randomColor = Colors.blue;
+  double randomWidth = 300;
+  double randomHeight = 100;
+
+  void changeColor() {
+    Random random = Random();
+    setState(() {
+      randomColor = Color.fromARGB(
+        255,
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+      );
+
+      randomWidth = (random.nextDouble() * 400) + 1;
+      randomHeight = (random.nextDouble() * 800) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 300,
-          height: 200,
-          // color: Colors.redAccent,
-          child: GraficoArco(porcentaje: 100),
+        child: GestureDetector(
+          onTap: changeColor,
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 400),
+            width: randomWidth,
+            height: randomHeight,
+            color: randomColor,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeColor,
+        child: const Icon(
+          Icons.add,
         ),
       ),
     );
